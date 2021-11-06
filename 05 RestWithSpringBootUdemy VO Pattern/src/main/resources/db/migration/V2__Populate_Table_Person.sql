6233c07cf81628e8489e7335ab5d40716798d39c
vinciusalmeidasantos <viniciusalmeida.as@gmail.com>
2021-11-06 13:13:41 -0300

    Criando Migrations

---
+++ 05 RestWithSpringBootUdemy VO Pattern/src/main/resources/db/migration/V2__Populate_Table_Person.sql
@@ -0,0 +1,7 @@
+INSERT INTO `person` (`id`, `address`, `first_name`, `gender`, `last_name`) VALUES
+	(1, 'Barueri - São Paulo - Brasil', 'Vinicius', 'Male', 'Santos'),
+	(2, 'Barueri - São Paulo - Brasil', 'Gabriela', 'Male', 'Santos'),
+	(5, 'Alphaville - São Paulo - Brasil', 'Felipe', 'Male', 'Santos'),
+	(7, 'Barueri - São Paulo - Brasil', 'Fernanda', 'Famale', 'Cardoso'),
+	(8, 'Barueri - São Paulo - Brasil', 'Joaquim', 'male', 'Freitas'),
+	(9, 'Barueri - São Paulo - Brasil', 'Marcos', 'male', 'Freitas');
